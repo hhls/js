83ae88e6481482681edd07302cd7585c2422a409
Halen.Wang <hhls@users.noreply.github.com>
2025-11-19 21:53:06 +0800

    Update order_query_cancel.js

--- order_query_cancel.js
+++ order_query_cancel.js
@@ -30,7 +30,7 @@ export const options = {
       startRate: 10,
       timeUnit: '1s',
       preAllocatedVUs: 50,
-      maxVUs: 500,
+      maxVUs: 1500,
       gracefulStop: '10s',              // 优雅停止时间
       stages: [
         { duration: '180s', target: 70 },
